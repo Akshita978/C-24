8f4ac6b1aa2572c62de347af9654eb538019e8ec
Akshita978 <76277505+Akshita978@users.noreply.github.com>
2021-01-05 17:43:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, box2, box3, box4;
+var box1, box2, box3, box4, box5;
 var pig1, pig2;
 var log1, log2, log3, log4;
 var bird;
@@ -17,6 +17,7 @@ function setup(){
     box2 = new Box(920, 320, 70, 70);
     box3 = new Box(700, 240, 70, 70);
     box4 = new Box(920, 240, 70, 70);
+    box5 = new Box(810, 160, 70, 70);
     ground = new Ground(600,height,1200, 20);
     pig1 = new Pig(810, 350);
     pig2 = new Pig(810, 220);
@@ -37,6 +38,7 @@ function draw(){
     box2.display();
     box3.display();
     box4.display();
+    box5.display();
     ground.display();
     pig1.display();
     pig2.display();
