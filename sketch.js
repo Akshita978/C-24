fc9507fdde8ad57ae4fa9996c48a303df2d1d9d2
Akshita978 <76277505+Akshita978@users.noreply.github.com>
2021-01-07 19:53:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,17 +15,22 @@ function setup(){
 
     box1 = new Box(700, 320, 70, 70);
     box2 = new Box(920, 320, 70, 70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810, 260, 300, PI/2);
+    
     box3 = new Box(700, 240, 70, 70);
     box4 = new Box(920, 240, 70, 70);
-    box5 = new Box(810, 160, 70, 70);
-    ground = new Ground(600,height,1200, 20);
-    pig1 = new Pig(810, 350);
     pig2 = new Pig(810, 220);
-    log1 = new Log(810, 260, 300, PI/2);
+    
     log2 = new Log(810, 180, 300, PI/2);
+    
+    box5 = new Box(810, 160, 70, 70);
     log3 = new Log(760, 120, 150, PI/7);
     log4 = new Log(870, 120, 150, -PI/7);
+    
     bird = new Bird(100, 100);
+    
+    ground = new Ground(600,height,1200, 20);
 }
 
 function draw(){
@@ -36,14 +41,16 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    box3.display();
-    box4.display();
-    box5.display();
     ground.display();
     pig1.display();
-    pig2.display();
     log1.display();
+    
+    box3.display();
+    box4.display();
+    pig2.display();
     log2.display();
+    
+    box5.display();
     log3.display();
     log4.display();
     bird.display();
